39c1b1ee1c3e01d11e80237148e6708faeb27b3d
sneakysunset <andrelauzier@gmail.com>
2022-11-09 15:36:35 +0100

    Supprimé vieux protos + 2 joueurs + correction bugs + Instantiation Objets + Meilleurs collisions
    Travail sur 1 semaine pour mettre le nouveau proto au niveau de l'ancien

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+//using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public enum Team { J1, J2 };
+    public Team playerType;
+    public Color colorJ1, colorJ2;
+    [HideInInspector] public Color col;
+    private Rigidbody rb;
+    public float jumpStrength;
+    public float normalYmaxInclinasion;
+    public bool groundCheck = false;
+    public float moveSpeed;
+    //public float accX, decX;
+    public float gravityStrength;
+    private float ogGravity;
+    public string horizontal;
+    KeyCode jumpKey;
+    [Range(-1f, 1f)] public float yNormalLineCollision;
+    private Vector3 prevVelocity;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        MeshRenderer pRend = GetComponentInChildren<MeshRenderer>();
+        ogGravity = rb.mass;
+        switch (playerType)
+        {
+            case Team.J1:
+                pRend.material.color = colorJ1;
+                col = colorJ1;
+                horizontal = "HorizontalJ1";
+                jumpKey = KeyCode.W;
+                break;
+            case Team.J2:
+                pRend.material.color = colorJ2;
+                col = colorJ2;
+                jumpKey = KeyCode.UpArrow;
+                horizontal = "HorizontalJ2";
+                break;
+            default:
+                return;
+        }
+    }
+
+    bool jumping;
+
+    private void Update()
+    {
+        if (groundCheck && Input.GetKeyDown(jumpKey))
+            jumping = true;
+    }
+
+    private void FixedUpdate()
+    {
+        
+        
+        Move();
+        if (jumping)
+        {
+            rb.mass = ogGravity;
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            jumping = false;
+        }
+        else if(!groundCheck)
+        {
+            rb.mass += Time.deltaTime * gravityStrength;
+        }
+        StartCoroutine(WaitForPhysics());
+    }
+
+    private void Move()
+    {
+        float horizontalAxis = Input.GetAxis(horizontal);
+        /*        float acc;
+                float acceleration = dir == Vector2.zero  ? acc = accX : acc = decX;
+                Vector2 move = Vector2.Lerp(Vector2.zero, dir * moveSpeed * Time.deltaTime, acc);
+                rb.velocity = move;*/
+        rb.velocity = new Vector2(horizontalAxis * moveSpeed * Time.deltaTime * 100, rb.velocity.y) ;
+       
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        groundCheck = false;
+        for (int i = 0; i < collision.contacts.Length; i++)
+        {
+            if (collision.contacts[i].normal.y > normalYmaxInclinasion) 
+            {
+                groundCheck = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "LineCollider" && collision.contacts[0].normal.y < yNormalLineCollision)
+        {
+            collision.gameObject.layer = LayerMask.NameToLayer("NoCollisionPlayer");
+            rb.velocity = prevVelocity;
+            //collision.collider.enabled = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        groundCheck = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "LineCollider" && other.gameObject.layer != LayerMask.NameToLayer("Collider" + playerType.ToString()))
+        {
+            other.gameObject.layer = LayerMask.NameToLayer("ColliderJ" + 1);
+            if(playerType == Team.J1) other.gameObject.layer = LayerMask.NameToLayer("ColliderJ" + 2);
+        }
+    }
+
+    IEnumerator WaitForPhysics()
+    {
+        yield return new WaitForFixedUpdate();
+        prevVelocity = rb.velocity;
+    }
+}
---
+++ Assets/Scripts/MeshGeneration.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+public class MeshGeneration : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]private float[] pointArray;
+    private List<List<Vector2>> lineList = new List<List<Vector2>>();
+    public List<Vector2> linelists;
+    private List<MeshFilter> meshsF = new List<MeshFilter>();
+    private List<MeshCollider> meshsC = new List<MeshCollider>();
+    private Color col;
+    public GameObject meshPrefab;
+    private CharacterController2D charC;
+    Transform meshFolder;
+    [Space(20)]
+    [Range(.001f, 1)] public float lineResolution = .5f;
+    public float lineBeginningX = -13f;
+    public float lineEndX = 13f;
+    //public float distanceToAddPoint = .5f;
+    public float distanceToCreateNewMesh = 2;
+    public float width = .3f;
+    public float lineYOffSet = 0;
+    public KeyCode renderKey;
+    #endregion
+
+    private void Start()
+    {
+        meshFolder = GameObject.FindGameObjectWithTag("MeshFolder").transform;
+        charC = GetComponent<CharacterController2D>();
+        pointArray = Utils_Mesh.GeneratePointArray(pointArray, lineBeginningX, lineEndX, lineResolution);
+        col = charC.col;
+        //InvokeRepeating("Inputer", 0, 0.005f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(renderKey)) input = true;
+        else input = false;
+    }
+
+    bool input;
+    private void FixedUpdate()
+    {
+        if (input) MeshCreator();
+    }
+
+    void MeshCreator()
+    {
+        int listIndex = Utils_Mesh.CurrentListIndex(pointArray, lineList, transform.position, distanceToCreateNewMesh, InstantiateMesh);
+        var list = lineList[listIndex];
+        list = list.OrderBy(v => v.x).ToList();
+        UpdatePointList(listIndex);
+
+        if (list.Count < 4) return;
+
+        Mesh m = new Mesh();
+        m.name = "trailMesh";
+
+        linelists.Clear() ;
+        linelists = list;
+
+        Utils_Mesh.UpdateMeshVertices(list, width, m);
+        Utils_Mesh.UpdateMeshTriangles(list.Count, m);
+        m.MarkDynamic();
+        m.Optimize();
+        m.OptimizeReorderVertexBuffer();
+        m.RecalculateBounds();
+        m.RecalculateNormals();
+        m.RecalculateTangents();    
+        meshsF[listIndex].mesh = m;
+        meshsC[listIndex].sharedMesh = null;
+        meshsC[listIndex].sharedMesh = m;
+        meshsF[listIndex].GetComponent<Renderer>().material.color = col;
+    }
+
+    public void UpdatePointList(int listIndex)
+    {
+        int closestVertexIndex = Utils_Mesh.ClosestPointInList(lineList[listIndex], transform.position.x, pointArray);
+        float closestVertexX = lineList[listIndex][closestVertexIndex].x;
+        //Enleve les doublons, il faut trouver une alternative
+        for (int i = 1; i < lineList[listIndex].Count; i++)
+        {
+            if (Mathf.Abs(lineList[listIndex][i].x - lineList[listIndex][i - 1].x) < lineResolution * .9f)
+            {
+                lineList[listIndex].RemoveAt(i);
+                return;
+            }
+        }
+        bool condition2 = Mathf.Abs(transform.position.x - closestVertexX) > lineResolution;
+      
+        if (condition2)
+            Utils_Mesh.AddPoints(pointArray ,lineList[listIndex], closestVertexX, transform.position, lineResolution);
+        else if (!condition2)
+            Utils_Mesh.UpdatePointsPos(lineList[listIndex], closestVertexIndex, transform.position, lineYOffSet);
+    }
+
+    void Inputer()
+    {
+        if (Input.GetKey(KeyCode.Space)) MeshCreator();
+    }
+
+    void InstantiateMesh()
+    {
+        GameObject temp = Instantiate(meshPrefab, meshFolder);
+        meshsF.Add(temp.GetComponent<MeshFilter>());
+        meshsC.Add(temp.GetComponent<MeshCollider>());
+        temp.name = "Mesh " + charC.playerType.ToString() + "N." + (meshsF.Count - 1);
+    }
+
+
+
+}
