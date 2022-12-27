19b6446cbdbd7ed6f5cc606948523babb3508569
sneakysunset <andrelauzier@gmail.com>
2022-12-27 18:38:33 +0100

    Changements camera

--- Assets/Scripts/Player/CamTrig.cs
+++ Assets/Scripts/Player/CamTrig.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class CamTrig : MonoBehaviour
 {
-    public GameObject CamContainer;
-    // Start is called before the first frame update
-    void Start()
+    public bool useTransformPos, useOldCamSize;
+    public Vector2 endPosition;
+    Vector3 endPos;
+    Transform camC;
+    public float camSizeDestination;
+    public float speed = 1;
+    public AnimationCurve animCurveCam, animCurveCamSize;
+    public IEnumerator camTransEnum;
+    Camera cam;
+    private void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        cam = Camera.main;
+        camC = cam.transform.parent;
+        if (useTransformPos) endPos = new Vector3(transform.position.x, transform.position.y, -1);
+        else endPos = new Vector3 (endPosition.x, endPosition.y, -1);
+        if (useOldCamSize) camSizeDestination = cam.orthographicSize;
     }
 
     void OnTriggerEnter2D (Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && Time.time > 1)
         {
-            Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -16);
+            //Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -16);
+            if (camTransEnum != null)
+            {
+                StopCoroutine(camTransEnum);
+            }
+            camTransEnum = cameraLerp(camC.position, cam.orthographicSize);
+            StartCoroutine(camTransEnum);
         }
+    }
+        
+    IEnumerator cameraLerp(Vector3 startPos, float startCamSize)
+    {
+        float i = 0;
+        while (i < 1)
+        {
+            i += Time.deltaTime * (1 / speed);
+            camC.position = Vector3.Lerp(startPos, endPos, animCurveCam.Evaluate(i));
+            cam.orthographicSize = Mathf.Lerp(startCamSize, camSizeDestination, animCurveCamSize.Evaluate(i));
+            yield return new WaitForEndOfFrame();
+        }
+        camC.position = endPos;
+        cam.orthographicSize = camSizeDestination;
+        camTransEnum = null;
+        yield return null;
     }
 }
